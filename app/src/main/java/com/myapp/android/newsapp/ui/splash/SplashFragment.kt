b38e3e694ccf25d048c9a496fc739059e89a7694
roddg <roddg@mail.ru>
2023-01-06 20:41:36 +0900

    lesson-2

---
+++ app/src/main/java/com/myapp/android/newsapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,23 @@
+package com.myapp.android.newsapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myapp.android.newsapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val mBinding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+        return mBinding.root
+    }
+}
